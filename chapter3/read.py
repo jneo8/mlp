93e346bac9b81fcd440faa905b43f9793d13eba2
jneo8 <james0910238727@gmail.com>
2017-04-26 00:37:25 +0800

    [ ] add cons [ ] add read.py

---
+++ chapter3/read.py
@@ -0,0 +1,35 @@
+import subprocess 
+import os
+from confs import logconf
+from scipy.io import loadmat
+
+logger = logconf.Logger(__file__).logger
+
+
+mnist_path = 'origin_data/mnist-original.mat'
+
+def download():
+    """Down mnist data from another site."""
+    subprocess.call(['wget', 'https://github.com/amplab/datascience-sp14/raw/master/lab7/mldata/mnist-original.mat', '-P', 'origin_data/'])
+    logger.info('Download success!')
+
+
+def read():
+    mnist_raw = loadmat(mnist_path)
+    mnist = {
+        'data': mnist_raw["data"].T,
+        'target': mnist_raw['label'][0],
+        'COL_NAMES': ["label", "data"],
+        'DESCR': 'mldata.org dataset: mnist-original', 
+    }
+    return mnist
+
+if __name__ == '__main__':
+    logger.info('read')
+    if os.path.exists(mnist_path):
+        logger.info('file exists.')
+    else:
+        logger.info('start download!')
+        download()
+    mnist = read()
+    print(mnist)
