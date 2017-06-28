8a2b5ec9dc9365bfd4a6862225946d169fe5094a
jneo8 <james851212@gmail.com>
2017-06-28 23:04:44 +0800

    normal_equalion_skl use skl

--- ch4/normal_equation.py
+++ ch4/normal_equation.py
@@ -1,9 +1,11 @@
 """The Normal Equation."""
+import os
 import subprocess
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 from confs import logconf
-import matplotlib.pyplot as plt
 
 logger = logconf.Logger(__file__).logger
 
@@ -12,7 +14,9 @@ def save_img_and_show(name):
     """save_img_and_show."""
     plt.savefig('img/{}'.format(name))
     plt.clf()
+    logger.info(name)
     subprocess.call(['catimg', '-f', 'img/{}.png'.format(name)])
+    os.remove('img/{}.png'.format(name))
 
 
 def normal_equation():
@@ -24,7 +28,44 @@ def normal_equation():
     logger.debug(y)
 
     plt.scatter(x, y)
-    save_img_and_show(name='p1')
+    save_img_and_show(name='randomly_generated_linear_dataset')
+
+    x_b = np.c_[np.ones((100, 1)), x]  # add x0 = 1 to each instance
+    theta_best = np.linalg.inv(x_b.T.dot(x_b)).dot(x_b.T).dot(y)
+    logger.info(
+        '\nEquation y = 4 + 3x0 + Gaussionnoise. theta_best {}'
+        .format(theta_best)
+    )
+
+    x_new = np.array([[0], [2]])
+    x_new_b = np.c_[np.ones((2, 1)), x_new]  # add x0= 1to each instance
+    y_predict = x_new_b.dot(theta_best)
+    logger.info('y_predict {}'.format(y_predict))
+
+    plt.plot(x_new, y_predict, "r-")
+    plt.plot(x, y, "b.")
+    plt.axis([0, 2, 0, 15])
+    save_img_and_show(name='linear regression model predictions')
+
+
+def normal_equation_skl():
+    """Linear regression model predictions use skl."""
+    x = 2 * np.random.rand(100, 1)
+    y = 4 + 3 * x + np.random.randn(100, 1)
+    x_new = np.array([[0], [2]])
+    lin_reg = LinearRegression()
+    lin_reg.fit(x, y)
+    logger.info('intercept_ {}'.format(lin_reg.intercept_))
+    logger.info('coef_ {}'.format(lin_reg.coef_))
+    y_predict = lin_reg.predict(x_new)
+    logger.info('predict {}'.format(y_predict))
+
+    plt.plot(x_new, y_predict, "r-")
+    plt.plot(x, y, "b.")
+    plt.axis([0, 2, 0, 15])
+    save_img_and_show(name='linear regression model predictions skl')
+
 
 if __name__ == '__main__':
     normal_equation()
+    normal_equation_skl()
