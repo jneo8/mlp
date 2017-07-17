67b297f4751ec8daad284802959b22605c33d516
jneo8 <james851212@gmail.com>
2017-07-17 23:11:14 +0800

    ch4 ridge_regression_with_skl

--- ch4/ridge_regression.py
+++ ch4/ridge_regression.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.pipeline import Pipeline
+from sklearn.linear_model import SGDRegressor
 
 from base import save_img_and_show
 from confs import logconf
@@ -14,8 +15,8 @@ logger = logconf.Logger(__file__).logger
 
 rnd.seed(42)
 m = 20
-X = 3 * rnd.rand(m, 1)
-y = 1 + 0.5 * X + rnd.randn(m, 1) / 1.5
+x = 3 * rnd.rand(m, 1)
+y = 1 + 0.5 * x + rnd.randn(m, 1) / 1.5
 x_new = np.linspace(0, 3, 100).reshape(100, 1)
 
 
@@ -34,7 +35,7 @@ def plot_model(model_class, polynomial, alphas, **model_kargs):
                 ("std_scaler", StandardScaler()),
                 ("regul_reg", model),
             ))
-        model.fit(X, y)
+        model.fit(x, y)
         y_new_regul = model.predict(x_new)
         lw = 2 if alpha > 0 else 1
         plt.plot(
@@ -44,7 +45,7 @@ def plot_model(model_class, polynomial, alphas, **model_kargs):
             linewidth=lw,
             label=r"$\alpha = {}$".format(alpha)
         )
-    plt.plot(X, y, "b.", linewidth=3)
+    plt.plot(x, y, "b.", linewidth=3)
     plt.legend(loc="upper left", fontsize=15)
     plt.xlabel("$x_1$", fontsize=18)
     plt.axis([0, 3, 0, 4])
@@ -62,6 +63,28 @@ def ridge_regression():
     save_img_and_show(name="ridge_regression_plot")
 
 
+def ridge_regression_with_skl():
+    """Ridge Regression with skl."""
+    # with closed-form solution
+    ridge_reg = Ridge(alpha=1, solver="cholesky")
+    ridge_reg.fit(x, y)
+
+    logger.info(
+        'ridge predict 1.5  : {}'
+        .format(ridge_reg.predict([[1.5]]))
+    )
+
+    # with Stochatic Gradient Descent
+    sgd_reg = SGDRegressor(penalty="l2")
+    sgd_reg.fit(x, y.ravel())
+    logger.info(
+        'sgd  predict 1.5 : {}'
+        .format(sgd_reg.predict([[1.5]]))
+    )
+
+
+
 if __name__ == '__main__':
     ridge_regression()
+    ridge_regression_with_skl()
 
